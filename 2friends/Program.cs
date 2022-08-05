1fdeaabd729d09aa4f0c78940065e073c1f4cee2
TimurGadisov <gadisovtmr@gmail.com>
2022-08-05 15:21:15 +0300

    Задача про двух собак

---
+++ 2friends/Program.cs
@@ -0,0 +1,20 @@
+﻿int count = 0, distance = 12000, firstFriendspeed = 1, secondFriendspeed = 2, dogSpeed = 5, friend = 2, time = 0;
+while (distance > 10)
+{
+  if (friend == 1) 
+  {
+    time = distance/(firstFriendspeed + dogSpeed);
+     friend = 2;
+    }
+  else
+  {
+    time = distance/(secondFriendspeed+dogSpeed);
+    friend=1;  
+  distance = distance - (firstFriendspeed+secondFriendspeed)*time;
+  count=count+1;
+  }
+}
+Console.WriteLine ("Собака пробежит" );
+Console.Write (count);
+Console.Write ("раз");
+
