d4ab738086c56cb8749dd88caaaf39b5e98eaf85
TimurGadisov <gadisovtmr@gmail.com>
2022-08-07 11:40:26 +0300

    Задача про четные числа от 1 до N

---
+++ task8/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine ("Введите  число");
+int number = Convert.ToInt32(Console.ReadLine ());
+int index = 1;
+while (index != number+1)
+{
+if ((index % 2)==0) 
+{
+    Console.Write (index);
+    Console.Write (",");
+
+}
+index = index +1;
+}
+
