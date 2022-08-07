7449c2c2e9034e9d07787e5e7081e3c9ae0faae9
TimurGadisov <gadisovtmr@gmail.com>
2022-08-07 11:26:14 +0300

    Задача на четное/нечетное

---
+++ task6/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine ("Введите  число");
+int number = Convert.ToInt32(Console.ReadLine ());
+if (number == 0)
+{
+    Console.WriteLine ("Вы ввели 0");
+} 
+else 
+if ((number % 2) == 0)
+{
+    Console.WriteLine ("Число четное");
+}
+else 
+{
+{
+    Console.WriteLine ("Число нечетное");
+}
+}
