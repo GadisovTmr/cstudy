a67aad2295fa552a27752609ff56b18cf0b4d224
TimurGadisov <gadisovtmr@gmail.com>
2022-08-07 11:19:12 +0300

    Нахождение большего из 3

---
+++ task4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine ("Введите первое число");
+int first = Convert.ToInt32(Console.ReadLine ());
+Console.WriteLine ("Введите  второе число");
+int second = Convert.ToInt32(Console.ReadLine ());
+Console.WriteLine ("Введите  третье число");
+int third = Convert.ToInt32(Console.ReadLine ());
+
+int max = first;
+
+if (max <= second)
+{
+    max = second;
+}
+if (max <= third)
+{
+    max = third;
+}
+Console.WriteLine ("Максимальное число ");
+Console.Write (max);
