f486a66c3f718f398c9b6538f6e8c5201105693e
TimurGadisov <gadisovtmr@gmail.com>
2022-08-07 11:10:09 +0300

    Нахождение большего из двух

---
+++ dz1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine ("Введите первое число");
+int first = Convert.ToInt32(Console.ReadLine ());
+Console.WriteLine ("Введите  второе число");
+int second = Convert.ToInt32(Console.ReadLine ());
+if (first>second)
+{
+ Console.Write ("Число ");
+ Console.Write (first); 
+ Console.Write (" больше");
+}
+if (first<second)
+{
+Console.Write ("Число ");
+ Console.Write (second); 
+ Console.Write (" больше");
+}
+if (first == second)
+{
+    Console.WriteLine ("Числа равны");
+}
